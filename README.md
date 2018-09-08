e8d1b864c8b1aa1f86e147a1c821f9330a64d9c9
Joel Evans <joeleoli99@gmail.com>
2018-09-08 03:50:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Frame
+A Spigot/Bukkit API library to create and update scoreboards.
+
+** Features
+* Non-flickering.
+* Supports up to 32 characters per line.
+* Extremely light weight.
+* Developer friendly. Setup your board in a few lines.
---
+++ src/main/java/me/joeleoli/frame/Frame.java
@@ -0,0 +1,105 @@
+package me.joeleoli.frame;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+
+@Getter
+public class Frame {
+
+	@Getter
+	private static Frame instance;
+
+	private JavaPlugin plugin;
+	private FrameAdapter adapter;
+	private Map<UUID, FrameBoard> boards;
+
+	public Frame(JavaPlugin plugin, FrameAdapter adapter) {
+		if (instance != null) {
+			throw new RuntimeException("Frame has already been instantiated!");
+		}
+
+		instance = this;
+
+		this.plugin = plugin;
+		this.adapter = adapter;
+		this.boards = new HashMap<>();
+
+		this.setup();
+	}
+
+	private void setup() {
+		this.plugin.getServer().getPluginManager().registerEvents(new FrameListener(), this.plugin);
+		this.plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, () -> {
+			for (Player player : this.plugin.getServer().getOnlinePlayers()) {
+				final FrameBoard board = this.boards.get(player.getUniqueId());
+
+				// This shouldn't happen, but just in case
+				if (board == null) {
+					continue;
+				}
+
+				final Scoreboard scoreboard = board.getScoreboard();
+				final Objective objective = board.getObjective();
+
+				// Update the title if needed
+				if (!objective.getDisplayName().equals(this.adapter.getTitle(player))) {
+					objective.setDisplayName(this.adapter.getTitle(player));
+				}
+
+				final List<String> newLines = this.adapter.getLines(player);
+
+				// Allow adapter to return null/empty list to display nothing
+				if (newLines == null || newLines.isEmpty()) {
+					board.getEntries().forEach(FrameBoardEntry::remove);
+					board.getEntries().clear();
+				} else {
+					// Reverse the lines because scoreboard scores are in descending order
+					Collections.reverse(newLines);
+
+					// Remove excessive amount of board entries
+					if (board.getEntries().size() > newLines.size()) {
+						for (int i = newLines.size(); i < board.getEntries().size(); i++) {
+							final FrameBoardEntry entry = board.getEntryAtPosition(i);
+
+							if (entry != null) {
+								entry.remove();
+							}
+						}
+					}
+
+					// Update existing entries / add new entries
+					for (int i = 0; i < newLines.size(); i++) {
+						FrameBoardEntry entry = board.getEntryAtPosition(i);
+
+						// If we're out of the new lines' range, it's safe to assume that
+						// we need to remove this entry.
+						final String line = newLines.get(i);
+
+						// If the entry is null, just create a new one.
+						// Creating a new FrameBoardEntry instance will add
+						// itself to the provided board's entries list.
+						if (entry == null) {
+							entry = new FrameBoardEntry(board, line);
+						}
+
+						// Update text, setup the team, and update the display values
+						entry.setText(line);
+						entry.setup();
+						entry.send(i);
+					}
+				}
+
+				player.setScoreboard(scoreboard);
+			}
+		}, 20L, 2L);
+	}
+
+}
