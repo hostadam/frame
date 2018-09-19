f0a368d52024a99cf31871bb09c5ca9adf7251cf
Joel Evans <joel@minexd.com>
2018-09-19 10:47:47 -0400

    replace test with actual example

--- example-plugin/src/main/java/me/joeleoli/frame/ExampleFramePlugin.java
+++ example-plugin/src/main/java/me/joeleoli/frame/ExampleFramePlugin.java
@@ -2,6 +2,7 @@ package me.joeleoli.frame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,20 +17,20 @@ public class ExampleFramePlugin extends JavaPlugin {
 		new Frame(this, new FrameAdapter() {
 			@Override
 			public String getTitle(Player player) {
-				return ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "Savage" + ChatColor.WHITE.toString() + ChatColor.BOLD.toString() + "Games";
+				return ChatColor.GOLD.toString() + "Frame Board Example";
 			}
 
 			@Override
 			public List<String> getLines(Player player) {
+				final int rand = ThreadLocalRandom.current().nextInt(500);
 				final List<String> toReturn = new ArrayList<>();
 
-				toReturn.add(ChatColor.GRAY.toString() + ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "---------------------------");
-				toReturn.add("Players: " + ChatColor.GOLD.toString() + "1");
-				toReturn.add("Host: " + ChatColor.GOLD.toString() + "joeleoli");
-				toReturn.add("Game Type: " + ChatColor.GOLD.toString() + "Cut NoClean");
-				toReturn.add(ChatColor.GRAY.toString() + ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "---------------------------");
-				toReturn.add(ChatColor.GRAY.toString() + ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "---------------------------");
-				toReturn.add(ChatColor.GRAY.toString() + ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "---------------------------");
+				toReturn.add("&eThis is an example board.");
+				toReturn.add("&dAs you can see, it supports");
+				toReturn.add("&dup to 32 characters per line.");
+				toReturn.add("&aDon't forget... Colors count");
+				toReturn.add("&btowards the character count.");
+				toReturn.add("Random number: " + rand);
 
 				return toReturn;
 			}
